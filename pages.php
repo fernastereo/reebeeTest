454f451f117c4d567c31945f9e1b0356a2dd682d
fernastereo <fernandoecueto@gmail.com>
2020-02-25 20:20:42 -0500

    Added script that accepts flyerID as a parameter and returns a list of pages

---
+++ pages.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if (!is_numeric($argv[1])) {
+  echo "The paremeter must be a number";
+  return 0;
+}
+
+$flyer_id = isset($argv[1]) ? $argv[1] : 0;
+
+$sqlPages = "select p.pagenumber, p.filename from pages p";
+            if ($flyer_id != 0) {
+              $sqlPages .= " where p.flyer_id = $flyer_id";
+            }
+            $sqlPages .= " order by p.pagenumber asc";
+
+$queryResult = $pdo->query($sqlPages);
+while ($row = $queryResult->fetch(PDO::FETCH_ASSOC)){
+  echo $row['pagenumber'] . ' | ' . $row['filename'] . "\n";
+}
